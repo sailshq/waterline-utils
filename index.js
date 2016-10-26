43073e630cfcfb970991b99472cedaa8b86ce1d7
Cody Stoltman <particlebanana@gmail.com>
2016-10-26 13:05:17 -0500

    move some join helpers to be private

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ module.exports = {
     converter: require('./lib/query/converter')
   },
   joins: {
-    planner: require('./lib/joins/planner'),
     queryCache: require('./lib/joins/query-cache'),
-    expandCriteria: require('./lib/joins/expand-criteria'),
     detectChildrenRecords: require('./lib/joins/detect-children-records')
   }
 };
