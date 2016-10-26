644a6e27cb7e55715b3fdea1886fcbc673533718
Cody Stoltman <particlebanana@gmail.com>
2016-10-26 12:44:26 -0500

    bring in the initial set of join helper functions

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ module.exports = {
     tokenizer: require('./lib/query/tokenizer'),
     analyzer: require('./lib/query/analyzer'),
     converter: require('./lib/query/converter')
+  },
+  joins: {
+    planner: require('./lib/joins/planner'),
+    queryCache: require('./lib/joins/query-cache'),
+    expandCriteria: require('./lib/joins/expand-criteria'),
+    detectChildrenRecords: require('./lib/joins/detect-children-records')
   }
 };
