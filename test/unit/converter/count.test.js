d0c3d0de462979381cca0d436d958277f98488b4
Cody Stoltman <particlebanana@gmail.com>
2016-11-18 17:20:36 -0600

    add support for count

---
+++ test/unit/converter/count.test.js
@@ -0,0 +1,34 @@
+var Test = require('../../support/convert-runner');
+
+describe('Converter :: ', function() {
+  describe('Count :: ', function() {
+    it('should generate a count query', function() {
+      Test({
+        criteria: {
+          model: 'user',
+          method: 'count',
+          criteria: {
+            where: {
+              firstName: 'Test',
+              lastName: 'User'
+            }
+          }
+        },
+        query: {
+          count: true,
+          from: 'user',
+          where: {
+            and: [
+              {
+                firstName: 'Test'
+              },
+              {
+                lastName: 'User'
+              }
+            ]
+          }
+        }
+      });
+    });
+  });
+});
