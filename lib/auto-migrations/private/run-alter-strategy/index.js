sailshq/waterline-utils

commit 5f3d4a158b9c8ae34eceeb845bd63300ab041144

In "alter" migration strategy, always sort backup data by primary key if available.

lib/auto-migrations/private/run-alter-strategy/index.js
@@ -77,6 +77,8 @@ module.exports = function runAlterStrategy(orm, cb) {
       tableDDLSpec[pkColumnName].primaryKey = true;
     }
 
+    // Make sure that the backup data is sorted by primary key (if the model has one).
+    var sort = primaryKey ? primaryKey.columnName + ' ASC' : [];
 
     //  ╔═╗╔═╗╔╦╗  ┌┐ ┌─┐┌─┐┬┌─┬ ┬┌─┐  ┌┬┐┌─┐┌┬┐┌─┐
     //  ║ ╦║╣  ║   ├┴┐├─┤│  ├┴┐│ │├─┘   ││├─┤ │ ├─┤
@@ -87,6 +89,7 @@ module.exports = function runAlterStrategy(orm, cb) {
       skipRecordVerification: true,
       skipExpandingDefaultSelectClause: true
     })
+    .sort(sort)
     .exec(function findCallback(err, backupRecords) {
       if (err) {
         // Ignore the error if it's an adapter error.  For example, this could error out

commit 15136cb0c01faee17227346f94fe00c5d6b30fda

Don't use column name for sorting
Fixes bug introduced in https://github.com/sailshq/waterline-utils/commit/5f3d4a158b9c8ae34eceeb845bd63300ab041144, because the "alter" strategy uses the Waterline "find" method, not lower-level adapter methods.

lib/auto-migrations/private/run-alter-strategy/index.js
@@ -78,7 +78,7 @@ module.exports = function runAlterStrategy(orm, cb) {
     }
 
     // Make sure that the backup data is sorted by primary key (if the model has one).
-    var sort = primaryKey ? primaryKey.columnName + ' ASC' : [];
+    var sort = primaryKey ? primaryKeyAttrName + ' ASC' : [];
 
     //  ╔═╗╔═╗╔╦╗  ┌┐ ┌─┐┌─┐┬┌─┬ ┬┌─┐  ┌┬┐┌─┐┌┬┐┌─┐
     //  ║ ╦║╣  ║   ├┴┐├─┤│  ├┴┐│ │├─┘   ││├─┤ │ ├─┤